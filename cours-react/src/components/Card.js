ba230cef4b99432fba8251f4143829d4b136fe2a
giraud-alexis <140321581+giraud-alexis@users.noreply.github.com>
2023-08-24 21:23:01 +0200

    ajout drapeau

---
+++ cours-react/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = ({ country }) => {
+  return (
+    <li className="card">
+      <div className="infos">
+        <h2>nom du pays</h2>
+      </div>
+    </li>
+  );
+};
+
+export default Card;
