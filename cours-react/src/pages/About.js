f8aa573c1192bb99f9b7f040d57d90492dcf77d9
giraud-alexis <140321581+giraud-alexis@users.noreply.github.com>
2023-08-24 15:05:37 +0200

    cours react

---
+++ cours-react/src/pages/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
+        aspernatur, accusantium impedit esse, facere ut alias animi amet
+        corrupti optio obcaecati iure repudiandae fugiat explicabo pariatur,
+        nihil molestias iusto eos. Quae eligendi mollitia sequi esse eveniet,
+        ducimus, dolores asperiores nam ipsam eos nostrum! Odio excepturi ea
+        earum eum quo non!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+        nesciunt voluptatum repellat sapiente asperiores incidunt officia saepe,
+        ipsam nemo doloremque, ex odio, consequatur temporibus maiores neque
+        unde! Facilis, est beatae?
+      </p>
+    </div>
+  );
+};
+
+export default About;
