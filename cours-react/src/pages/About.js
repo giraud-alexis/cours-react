5d9f4c3a728e0bda8c374a08594b1a9ab1e56e33
giraud-alexis <140321581+giraud-alexis@users.noreply.github.com>
2023-08-24 15:58:55 +0200

    ajout navbar

--- cours-react/src/pages/About.js
+++ cours-react/src/pages/About.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import Navigation from "../components/Navigation";
 
 const About = () => {
   return (
     <div>
+      <Navigation />
       <h1>A propos</h1>
       <br />
       <p>
